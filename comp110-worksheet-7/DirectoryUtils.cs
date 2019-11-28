df39f9d1be830b9b28537e7218fc00174ec0bfe4
Morgan <MP232027@falmouth.ac.uk>
2019-11-28 14:05:57 +0000

    Implement method computations (tests are yet to be updated from upstream).
    * Implement GetTotalSize (Lines 27 to 33)
    * Implement CountFiles (Line 38) - Lazy method of file count
    * Implement GetDepth (Lines 44 to 52) - Recursive method depth, yet to be tested
    * Implement GetSmallestFile (Lines 58 to 74)
    * Implement GetLargestFile (Lines 80 to 96)
    * Implement GetFilesOfSize (Lines 102 to 109)
    
    As said above, tests are yet to be merged with @edpowley new additions at 32f258f of master to retarget the test folder as a result of VS19 changes to how tests behave and where they are stored.

--- comp110-worksheet-7/DirectoryUtils.cs
+++ comp110-worksheet-7/DirectoryUtils.cs
@@ -24,37 +24,89 @@ namespace comp110_worksheet_7
 		// Return the total size, in bytes, of all the files below the given directory
 		public static long GetTotalSize(string directory)
 		{
-			throw new NotImplementedException();
-		}
+            long total = 0;
+            foreach (FileInfo file in new DirectoryInfo(directory).EnumerateFiles())
+            {
+                total += file.Length;
+            }
+            return total;
+        }
 
 		// Return the number of files (not counting directories) below the given directory
 		public static int CountFiles(string directory)
 		{
-			throw new NotImplementedException();
+            return new DirectoryInfo(directory).GetFiles().Length;
 		}
 
 		// Return the nesting depth of the given directory. A directory containing only files (no subdirectories) has a depth of 0.
 		public static int GetDepth(string directory)
 		{
-			throw new NotImplementedException();
+            int depth = 0;
+            string[] directories = Directory.GetDirectories(directory);
+            if (directories.Length == 0) return 0;
+            foreach(string dir in directories)
+            {
+                int _depth = GetDepth(dir);
+                if (_depth > depth) depth = _depth;
+            }
+            return depth;
 		}
 
 		// Get the path and size (in bytes) of the smallest file below the given directory
 		public static Tuple<string, long> GetSmallestFile(string directory)
 		{
-			throw new NotImplementedException();
+            string fileName = null;
+            long fileSize = -1;
+            foreach(FileInfo file in new DirectoryInfo(directory).EnumerateFiles())
+            {
+                if(fileName == null || fileSize == -1)
+                {
+                    fileName = file.Name;
+                    fileSize = file.Length;
+                    continue;
+                }
+                if(file.Length < fileSize)
+                {
+                    fileName = file.Name;
+                    fileSize = file.Length;
+                }
+            }
+            return new Tuple<string, long>(fileName, fileSize);
 		}
 
 		// Get the path and size (in bytes) of the largest file below the given directory
 		public static Tuple<string, long> GetLargestFile(string directory)
 		{
-			throw new NotImplementedException();
+            string fileName = null;
+            long fileSize = -1;
+            foreach (FileInfo file in new DirectoryInfo(directory).EnumerateFiles())
+            {
+                if (fileName == null || fileSize == -1)
+                {
+                    fileName = file.Name;
+                    fileSize = file.Length;
+                    continue;
+                }
+                if (file.Length > fileSize)
+                {
+                    fileName = file.Name;
+                    fileSize = file.Length;
+                }
+            }
+            return new Tuple<string, long>(fileName, fileSize);
 		}
 
 		// Get all files whose size is equal to the given value (in bytes) below the given directory
 		public static IEnumerable<string> GetFilesOfSize(string directory, long size)
 		{
-			throw new NotImplementedException();
+            List<string> list = new List<string>();
+            foreach (FileInfo file in new DirectoryInfo(directory).GetFiles())
+            {
+                if (file.Length == size)
+                    list.Add(file.FullName);
+            }
+
+            return list;
 		}
 	}
 }
